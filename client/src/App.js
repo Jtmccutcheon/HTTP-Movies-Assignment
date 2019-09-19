da27d591ccf0c976f26aecf1718c829aa91d5df6
jtmccutcheon <jt.mccutcheon@live.com>
2019-09-19 19:27:52 -0400

    manage state in app and props getting to form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+
 import Movie from "./Movies/Movie";
+import UpdateForm from './Movies/UpdateForm'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movie, setMovie] = useState([])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(res => {
+        // console.log(res)
+        setMovie(res.data)
+      })
+      .catch(err => console.log(err, 'error'))
+  }, [])
 
   return (
     <>
@@ -19,6 +33,12 @@ const App = () => {
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
+        }}
+      />
+      <Route
+        path='/update-movie/:id'
+        render={props => {
+          return <UpdateForm {...props} movie={movie} setMovie={setMovie} />
         }}
       />
     </>
---
+++ client/src/Movies/UpdateForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const UpdateForm = props => {
+    console.log(props)
+    return (
+        <form>
+            <input></input>
+        </form>
+    )
+}
+
+export default UpdateForm
